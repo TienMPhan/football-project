c520f53dead6c9aede69a488891ce316a5fc4da9
Tien Phan <minhtien882000@gmail.com>
2019-08-07 21:24:09 -0500

    modify code

--- 1protein/MC3D_1.cpp
+++ 1protein/MC3D_1.cpp
@@ -16,14 +16,11 @@ using namespace std;
 
 
 //functions
-// void printCords();
-// void printConvergence();
+bool startPlacement(int length);
+bool checkSpace(int X, int Y, int Z, int length);
+void placeBlock(int X,int Y, int Z, int currentBlock, int length);
 
-bool startPlacement();
-bool checkSpace(int X, int Y, int Z);
-void placeBlock(int X,int Y, int Z, int currentBlock);
-
-bool energyCheck(int bid, int xrand, int yrand, int zrand, float energyConstant);
+bool energyCheck(int bid, int xrand, int yrand, int zrand, float energyConstant, int length);
 
 int randomBlock();
 int randint(int lower, int upper); // (inclusive, inclusive)
@@ -34,23 +31,19 @@ int ypos(int Y);
 int zpos(int Z);
 
 void updatepos(int bid, int X, int Y, int Z);
-bool moveCheck(int bid, int xrand, int yrand, int zrand);
-void moveBlock(int bid, int xrand, int yrand, int zrand);
-
-int findBonds(int bondThresh);
-
+bool moveCheck(int bid, int xrand, int yrand, int zrand, int length);
+void moveBlock(int bid, int xrand, int yrand, int zrand, int length);
 
 int moveDistance = 4;
 long long int counter = 1;
-long long int iderations = 2000000000;
+// long long int iderations = 2000000000;
 
-const int length = 7;
 const int blocks = 20000;
 const int dimension = 4;
 // const float energyConstant = .8;
-bool firstTimeOutputted = true;
-int savedFileNumber;
-int setseed = false;
+// bool firstTimeOutputted = true;
+// int savedFileNumber;
+// int setseed = false;
 int cord[blocks][dimension];
 int array[Xmax][Ymax][Zmax];
 int visibleArray [Xmax][Ymax];
@@ -63,13 +56,15 @@ std::mt19937_64 dre(rd());
 
 int main(int argc, char* argv[]){
 
-	if (argc != 3)
+	if (argc != 5)
         printf("Err: %s needs input paramenter!\n", argv[0]);
 
-    float energyConstant = strtod(argv[1], nullptr);
-    int id = strtod(argv[2], nullptr);
+    float energyConstant = atof(argv[1]);
+    int length = stoi(argv[2]);
+    long long int iderations = atoll(argv[3]);
+    int id = atoi(argv[4]);
 
-	if(startPlacement() == false){
+	if(startPlacement(length) == false){
 		cout<<"There was not enough space to place all the proteins"<<endl;
 		return 0;
 	}
@@ -79,20 +74,19 @@ int main(int argc, char* argv[]){
 	int yrand;
 	int zrand;
 
-
 	while(counter <= iderations){
 		int bid = randint(1, blocks);
 		xrand = randint(-(2*moveDistance), (2*moveDistance));
 		yrand = randint(-moveDistance, moveDistance);
 		zrand = randint(-moveDistance, moveDistance);
-		if(moveCheck(bid, xrand, yrand, zrand) == true){
-			if(energyCheck(bid, xrand, yrand, zrand, energyConstant) == true){
-				moveBlock(bid, xrand, yrand, zrand);
+		if(moveCheck(bid, xrand, yrand, zrand, length) == true){
+			if(energyCheck(bid, xrand, yrand, zrand, energyConstant, length) == true){
+				moveBlock(bid, xrand, yrand, zrand, length);
 				updatepos(bid, xrand, yrand, zrand);
 
 				if(counter % iderations == 0){
 					char fileName[80];
-					snprintf(fileName, 80, "Arr-%f-%d.txt", energyConstant, id);
+					snprintf(fileName, 80, "Arr-%.2f-%d.txt", energyConstant, id);
 					FILE *file = fopen(fileName, "w");
 					// fwrite(array, sizeof(array), 1, file);
 					for(int i = 0; i < Zmax; i++){
@@ -124,92 +118,8 @@ int main(int argc, char* argv[]){
 	return 0;
 }
 
-
-// void printCords(){
-// 	ifstream infile;
-// 	ofstream file;
-// 	int fileNumber = -1;
-// 	char val;
-// 	stringstream fileName;
-// 	do{
-// 		infile.close();
-// 		stringstream fileName;
-// 		fileNumber++;
-// 		fileName << "3DCordData("<< fileNumber << ").txt";
-// 		infile.open(fileName.str().c_str());
-// 		val = ' ';
-// 		infile >> val;
-// 	}while(val == 'X');
-// 	infile.close();
-// 	fileName << "3DCordData("<< fileNumber << ").txt";
-// 	file.open(fileName.str().c_str());
-
-// 	file<<"Xmax = "<<Xmax<<endl;
-// 	file<<"Ymax = "<<Ymax<<endl;
-// 	file<<"Zmax = "<<Zmax<<endl;
-// 	file<<"Proteins = "<<blocks<<endl;
-// 	file<<"Protein Length = "<<length<<endl;
-// 	file<<"Bond Energy = "<<energyConstant<<endl;
-// 	file<<"Iderations = "<<iderations<<endl;
-// 	file<<"Proteins With Less Than 1 Bond = "<<findBonds(1)<<endl;
-// 	file<<"Proteins With Less Than 2 Bond = "<<findBonds(2)<<endl;
-// 	file<<"Proteins With Less Than 3 Bond = "<<findBonds(3)<<endl;
-// 	file<<"Proteins With Less Than 4 Bond = "<<findBonds(4)<<endl;
-// 	file<<"Proteins With Less Than 5 Bond = "<<findBonds(5)<<endl;
-// 	file<<"Proteins With Less Than 6 Bond = "<<findBonds(6)<<endl;
-// 	file<<"Proteins With Less Than 7 Bond = "<<findBonds(7)<<endl;
-// 	file<<"Proteins With Less Than 8 Bond = "<<findBonds(8)<<endl;
-
-
-// 	for(int Z = 0; Z<Zmax; Z++){
-// 		for(int Y = 0; Y<Ymax; Y++){
-// 			for(int X = 0; X<Xmax; X++){
-// 				if(array[X][Y][Z] != 0){
-// 					file<<"1 ";
-// 				}
-// 				else{
-// 					file<<"0 ";
-// 				}
-// 			}
-// 			file<<endl;
-// 		}
-// 		file<<endl;
-// 	}
-// }
-
-// void printConvergence(){
-// 	ofstream file;
-// 	ifstream infile;
-// 	int fileNumber = -1;
-// 	char val;
-// 	if(firstTimeOutputted == true){
-// 		do{
-// 			infile.close();
-// 			stringstream fileName;
-// 			fileNumber++;
-// 			fileName << "ConvergenceData("<< fileNumber << ").txt";
-// 			infile.open(fileName.str().c_str());
-// 			val = ' ';
-// 			infile >> val;
-// 		}while(val == 'C');
-// 		infile.close();
-// 		stringstream fileName;
-// 		fileName << "ConvergenceData("<< fileNumber << ").txt";
-// 		file.open(fileName.str().c_str());
-// 		file<<"ConvergenceData"<<endl<<"IterationsCompleted   blocks<1   blocks<2   blocks<3   blocks<4   blocks<5   blocks<6   blocks<7   blocks<8"<<endl;
-// 		firstTimeOutputted = false;
-// 		file.close();
-// 		savedFileNumber = fileNumber;
-// 	}
-// 	stringstream fileName;
-// 	fileName << "ConvergenceData("<< savedFileNumber <<").txt";
-// 	file.open(fileName.str().c_str(), std::ios::app);
-
-// 	file<<counter<<" "<<findBonds(1)<<" "<<findBonds(2)<<" "<<findBonds(3)<<" "<<findBonds(4)<<" "<<findBonds(5)<<" "<<findBonds(6)<<" "<<findBonds(7)<<" "<<findBonds(8)<<endl;
-// }
-
 //Initial Placement Functions
-bool startPlacement(){
+bool startPlacement(int length){
 	int reset = 0;
 	while(reset<5){
 		//Exits the function if there are too many fails
@@ -233,13 +143,13 @@ bool startPlacement(){
 			int x = randint(0, Xmax-1);
 			int y = randint(0, Ymax-1);
 			int z = randint(0, Zmax-1);
-			if(checkSpace(x,y,z) == true){
+			if(checkSpace(x, y, z, length) == true){
 				cord[blocksPlaced][0] = blocksPlaced+1;
 				cord[blocksPlaced][1] = x;
 				cord[blocksPlaced][2] = y;
 				cord[blocksPlaced][3] = z;
 				blocksPlaced++;
-				placeBlock(x, y, z, blocksPlaced);
+				placeBlock(x, y, z, blocksPlaced, length);
 				fails = 0;
 			}
 			else{
@@ -256,7 +166,7 @@ bool startPlacement(){
 	}
 	return false;
 }
-bool checkSpace(int X, int Y, int Z){
+bool checkSpace(int X, int Y, int Z, int length){
 	for(int i = 0; i<length; i++){
 		if(array[xpos(X+i)][Y][Z] != 0){
 			return false;
@@ -264,13 +174,13 @@ bool checkSpace(int X, int Y, int Z){
 	}
 	return true;
 }
-void placeBlock(int X, int Y, int Z, int currentBlock){
+void placeBlock(int X, int Y, int Z, int currentBlock, int length){
 	for(int i =0; i < length; i++){
 		array[xpos(X+i)][Y][Z] = currentBlock;
 	}
 }
 
-bool energyCheck(int bid, int xrand, int yrand, int zrand, float energyConstant){
+bool energyCheck(int bid, int xrand, int yrand, int zrand, float energyConstant, int length){
 
 	float energy1 = 0;
 	float energy2 = 0;
@@ -305,18 +215,6 @@ bool energyCheck(int bid, int xrand, int yrand, int zrand, float energyConstant)
 			}
 		}
 	}
-//	if(array[xpos(Xval-1)][Yval][Zval] != 0 && array[xpos(Xval-1)][Yval][Zval] != bid){
-//		energy1 += 0;
-//	}
-//	if(array[xpos(Xval+length)][Yval][Zval] != 0 && array[xpos(Xval+length)][Yval][Zval] != bid){
-//		energy1 += 0;
-//	}
-//	if(array[xpos(Xval-1+xrand)][ypos(Yval+yrand)][zpos(Zval+zrand)] != 0 && array[xpos(Xval-1+xrand)][ypos(Yval+yrand)][zpos(Zval+zrand)] != bid){
-//		energy2 += 0;
-//	}
-//	if(array[xpos(Xval+length+xrand)][ypos(Yval+yrand)][zpos(Zval+zrand)] != 0 && array[xpos(Xval+length+xrand)][ypos(Yval+yrand)][zpos(Zval+zrand)] != bid){
-//		energy2 += 0;
-//	}
 
 	float tenergy = energy2 - energy1;
 	if(tenergy >=0){
@@ -382,7 +280,7 @@ void updatepos(int bid, int xrand, int yrand, int zrand){
 	cord[bid-1][2] = ypos(Y+yrand);
 	cord[bid-1][3] = zpos(Z+zrand);
 }
-bool moveCheck(int bid, int xrand, int yrand, int zrand){
+bool moveCheck(int bid, int xrand, int yrand, int zrand, int length){
 	int X = cord[bid-1][1];
 	int Y = cord[bid-1][2];
 	int Z = cord[bid-1][3];
@@ -393,7 +291,7 @@ bool moveCheck(int bid, int xrand, int yrand, int zrand){
 	}
 	return true;
 }
-void moveBlock(int bid, int xrand, int yrand, int zrand){
+void moveBlock(int bid, int xrand, int yrand, int zrand, int length){
 	int X = cord[bid-1][1];
 	int Y = cord[bid-1][2];
 	int Z = cord[bid-1][3];
@@ -405,59 +303,3 @@ void moveBlock(int bid, int xrand, int yrand, int zrand){
 	}
 }
 
-int findBonds(int bondThresh){
-	int totalBonds=0;
-	int listOfInteractions[8];
-
-	for(int bid = 1; bid <= blocks; bid++){
-		int listval = 0;
-		for(int i=0; i<8; i++){
-			listOfInteractions[i] = 0;
-		}
-
-		int bondsPerBlock = 0;
-		int X = cord[bid-1][1];
-		int Y = cord[bid-1][2];
-		int Z = cord[bid-1][3];
-		for(int yval = -1; yval<=1; yval+=2){
-			for(int xval = 0; xval<length; xval++){
-				if(array[xpos(X+xval)][ypos(Y+yval)][zpos(Z)] != 0){
-					bool test = true;
-					for(int previous = 0; previous < 8; previous++){
-						if(array[xpos(X+xval)][ypos(Y+yval)][zpos(Z)] == listOfInteractions[previous]){
-							test = false;
-							break;
-						}
-					}
-					if(test == true){
-						bondsPerBlock++;
-						listOfInteractions[listval] = array[xpos(X+xval)][ypos(Y+yval)][zpos(Z)];
-						listval++;
-					}
-				}
-			}
-		}
-		for(int zval = -1; zval<=1; zval+=2){
-			for(int xval = 0; xval<length; xval++){
-				if(array[xpos(X+xval)][ypos(Y)][zpos(Z+zval)] != 0){
-					bool test = true;
-					for(int previous = 0; previous < 8; previous++){
-						if(array[xpos(X+xval)][ypos(Y)][zpos(Z+zval)] == listOfInteractions[previous]){
-							test = false;
-							break;
-						}
-					}
-					if(test == true){
-						bondsPerBlock++;
-						listOfInteractions[listval] = array[xpos(X+xval)][ypos(Y)][zpos(Z+zval)];
-						listval++;
-					}
-				}
-			}
-		}
-		if(bondsPerBlock < bondThresh){
-			totalBonds++;
-		}
-	}
-	return totalBonds;
-}
